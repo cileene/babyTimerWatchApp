73e90fb58d136b8d0db80562bd77a41e6da6db0a
cileene <106276740+cileene@users.noreply.github.com>
2023-11-06 20:34:34 +0100

    Initial Commit

---
+++ xcodeWatchTest Watch App/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  xcodeWatchTest Watch App
+//
+//  Created by Nick Jerlung on 06/11/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ xcodeWatchTest Watch App/xcodeWatchTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  xcodeWatchTestApp.swift
+//  xcodeWatchTest Watch App
+//
+//  Created by Nick Jerlung on 06/11/2023.
+//
+
+import SwiftUI
+
+@main
+struct xcodeWatchTest_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
