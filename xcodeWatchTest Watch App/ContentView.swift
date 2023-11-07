f59cae52f0038c19bab2e9ee08253e588def27b5
cileene <106276740+cileene@users.noreply.github.com>
2023-11-07 10:59:06 +0100

    Playing around with copilot in xcode

--- xcodeWatchTest Watch App/ContentView.swift
+++ xcodeWatchTest Watch App/ContentView.swift
@@ -30,6 +30,7 @@ struct ContentView: View {
                     .background(Color.red)
                     .foregroundColor(.black)
                     .cornerRadius(10)
+                
             }
 
             Text(timeSince(timeInterval: lastFeeding))
