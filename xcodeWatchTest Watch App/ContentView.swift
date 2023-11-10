dc959987741c08aaff2bcb9c673a08c535b54d35
cileene <106276740+cileene@users.noreply.github.com>
2023-11-10 11:14:51 +0100

    Added the last interval to the ui

--- xcodeWatchTest Watch App/ContentView.swift
+++ xcodeWatchTest Watch App/ContentView.swift
@@ -8,8 +8,10 @@
 import SwiftUI
 
 struct ContentView: View {
-    @AppStorage("lastFeeding") private var lastFeeding: Double = 0
-    @AppStorage("lastDiaperChange") private var lastDiaperChange: Double = 0
+    @AppStorage("lastFeeding 0") private var lastFeeding0: Double = 0
+    @AppStorage("lastFeeding 1") private var lastFeeding1: Double = 0
+    @AppStorage("lastDiaperChange 0") private var lastDiaperChange0: Double = 0
+    @AppStorage("lastDiaperChange 1") private var lastDiaperChange1: Double = 0
     @State private var currentTime = Date()
     
     // set the babys name
@@ -23,28 +25,42 @@ struct ContentView: View {
             Image(systemName: "heart")
                 .imageScale(.large)
                 .foregroundStyle(.red)
+                .padding(.top, -25)
+                
             
             Text(babyName)
+                .padding(.top, -10)
             
             Button(action: {
-                self.lastFeeding = Date().timeIntervalSince1970
+                lastFeeding1 = lastFeeding0
+                self.lastFeeding0 = Date().timeIntervalSince1970
                 print(babyName,"just finished eating!")
+                // print interval to console
+                print(timeSince(timeInterval: lastFeeding1))
             }) {
                 Text("Feed")
                     .padding()
                     .background(Color.red)
                     .foregroundColor(.black)
                     .cornerRadius(10)
-                
             }
 
-            Text(timeSince(timeInterval: lastFeeding))
-                .padding()
+            Text("Fed \(timeSince(timeInterval: lastFeeding0)) min ago")
+                .padding(-3)
+                .foregroundColor(.red)
+            
+            Text("Interval \(timeSince(timeInterval: lastFeeding1)) min")
+                .font(.caption2)
+                .fontWeight(.thin)
+                .padding(-3)
                 .foregroundColor(.red)
 
             Button(action: {
-                self.lastDiaperChange = Date().timeIntervalSince1970
+                lastDiaperChange1 = lastDiaperChange0
+                self.lastDiaperChange0 = Date().timeIntervalSince1970
                 print(babyName,"just got a fresh diaper!")
+                // print interval to console
+                print(timeSince(timeInterval: lastDiaperChange1))
             }) {
                 Text("Change")
                     .padding()
@@ -53,9 +69,16 @@ struct ContentView: View {
                     .cornerRadius(10)
             }
 
-            Text(timeSince(timeInterval: lastDiaperChange))
-                .padding()
+            Text("Changed \(timeSince(timeInterval: lastDiaperChange0)) min ago")
+                .padding(-3)
                 .foregroundColor(.red)
+            Text("Interval \(timeSince(timeInterval: lastDiaperChange1)) min")
+                .font(.caption2)
+                .fontWeight(.thin)
+                .padding(-3)
+                .foregroundColor(.red)
+            
+          
         }
         .padding()
         .onReceive(timer) { _ in
@@ -67,7 +90,7 @@ struct ContentView: View {
         let date = Date(timeIntervalSince1970: timeInterval)
         let timeInterval = currentTime.timeIntervalSince(date)
         let minutes = Int(timeInterval / 60)
-        return timeInterval > 0 ? "\(minutes) min ago" : "N/A"
+        return timeInterval > 0 ? "\(minutes)" : "N/A"
     }
 }
 
